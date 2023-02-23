250dfa894e47f41bae6e666bffdf31d65d0d8a0b
Anand Patel <anandpp2001@gmail.com>
2023-02-23 14:03:27 -0700

    setup backend

---
+++ backend/src/main/java/nicole/anand/rizeup/rizeup/RizeupApplication.java
@@ -0,0 +1,13 @@
+package nicole.anand.rizeup.rizeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizeupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nicole/anand/rizeup/rizeup/RizeupApplicationTests.java
@@ -0,0 +1,13 @@
+package nicole.anand.rizeup.rizeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
