6de8cd59e5ec921bc2772e388834ca27e724adf8
Anand Patel <anandpp2001@gmail.com>
2023-04-06 19:17:01 -0600

    navbar

--- frontend/src/components/shared/main-body.tsx
+++ frontend/src/components/shared/main-body.tsx
@@ -1,11 +1,18 @@
+import SideNavBar from "./nav/side-nav-bar";
+
 interface MainBodyProps {
   children: React.ReactNode;
 }
 
 const MainBody: React.FC<MainBodyProps> = ({ children }: MainBodyProps) => {
   return (
-    <div className="grid grid-cols-1 grow h-full bg-gradient-to-t from-slate-800 to-stone-900">
-      <div className="relative px-6 py-4 flex flex-col">{children}</div>
+    <div className="grid grid-cols-1 grow h-full">
+      <div className="relative p-2 flex flex-row">
+        <SideNavBar />
+        <div className="relative px-6 py-4 flex flex-col bg-[#f4f4fc] w-full rounded-3xl">
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
